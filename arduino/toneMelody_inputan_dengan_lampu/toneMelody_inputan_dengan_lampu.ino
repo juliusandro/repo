41c7ff3e5edb9342e13edf93c269910a54a50096
juliusandro <jandropr78@gmail.com>
2016-04-21 15:00:31 +0700

    tambah arduino

---
+++ arduino/toneMelody_inputan_dengan_lampu/toneMelody_inputan_dengan_lampu.ino
@@ -0,0 +1,40 @@
+
+#include "pitches.h"
+
+// notes in the melody:
+int melody[] = {
+  NOTE_C7, NOTE_D7, NOTE_E7, NOTE_F7, NOTE_G7, NOTE_A7, NOTE_B7, NOTE_C8
+};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+  1, 2, 3, 1, 3, 1, 3
+};
+
+void setup() {
+  // iterate over the notes of the melody:
+  Serial.begin(9600);
+ pinMode(2, OUTPUT);
+ pinMode(1, OUTPUT);
+  }
+
+
+void loop() {
+  // no need to repeat the melody.
+ int a= Serial.parseInt();
+ if(a>0){
+ tone(8,melody[a]-1,200);
+ }
+ if(a%2==0){
+  digitalWrite(2,LOW);
+  digitalWrite(1,HIGH);
+  
+  }
+  else{
+     digitalWrite(1,LOW);
+    digitalWrite(2,HIGH);
+ 
+    }
+ delay(100);
+  
+}
